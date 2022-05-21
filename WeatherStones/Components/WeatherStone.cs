c707d65bf6a89acca3fff7b40e6cc6e4c85f0eaf
GoldenJude <pavel.skrabal98@seznam.cz>
2022-05-21 10:32:14 +0200

    Add project files.

---
+++ WeatherStones/Components/WeatherStone.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using UnityEngine;
+
+namespace WeatherStones.Components
+{
+    public class WeatherStone : MonoBehaviour, Interactable, TextReceiver
+    {
+        private ZNetView znv;
+        private EnvZone env;
+        private MeshRenderer renderer;
+
+        public float speed = 1;
+        public float amplitude = 1;
+        private Color col = Color.white;
+
+        private readonly bool defaultEnabled = true;
+        private readonly string zdo_enabled = "enabled";
+        private readonly string zdo_env = "env";
+
+        private bool Valid => znv != null && znv.IsValid();
+
+        private void Awake()
+        {
+            znv = GetComponent<ZNetView>();
+
+            if (!Valid) return;
+
+            env = GetComponentInChildren<EnvZone>(true);
+            renderer = GetComponentInChildren<MeshRenderer>(true);
+
+            znv.Register(nameof(RPC_SetEnv), new Action<long, string>(RPC_SetEnv));
+            znv.Register(nameof(RPC_UpdateStone), new Action<long>(RPC_UpdateStone));
+
+            this.znv.InvokeRPC(nameof(RPC_UpdateStone));
+        }
+
+        private void FixedUpdate()
+        {
+            renderer?.material.SetColor("_EmissionColor", col * ((Mathf.Sin(Time.time * speed) * amplitude) + 0.5f));
+        }
+
+        public void RPC_UpdateStone(long sender)
+        {
+            UpdateColor();
+            UpdateEnv();
+            GetComponentInChildren<SphereCollider>(true).radius = Main.radius.Value;
+        }
+
+        private void UpdateEnv()
+        {
+            if (!Valid) return;
+
+            env.m_environment = GetText();
+        }
+
+        private void UpdateColor()
+        {
+            if (!Valid) return;
+
+            if (!znv.GetZDO().GetBool(zdo_enabled, defaultEnabled)) col = Color.black;
+
+            var envSetup = EnvMan.instance?.GetEnv(GetText());
+            if (envSetup != null)
+            {
+                col = envSetup.m_ambColorDay;
+            }
+            else col = Color.black;
+
+            renderer.material.SetColor("_EmissionColor", col);
+        }
+
+        public void ToggleActive(bool status)
+        {
+            if (!Valid) return;
+
+            znv.GetZDO().Set(zdo_enabled, status);
+            znv.InvokeRPC(nameof(RPC_UpdateStone));
+        }
+
+        public bool Interact(Humanoid user, bool hold, bool alt)
+        {
+            if (hold) return false;
+
+            if(!PrivateArea.CheckAccess(transform.position, 0f, true, false))
+            {
+                user.Message(MessageHud.MessageType.Center, "$piece_noaccess", 0, null);
+                return true;
+            }
+            TextInput.instance.RequestText(this, "$WS_stone_big", 20);
+
+            return true;
+        }
+
+        public void SetText(string text)
+        {
+            if (!Valid) return;
+
+            this.znv.InvokeRPC(nameof(RPC_SetEnv), new object[] { text });
+        }
+
+        private void RPC_SetEnv(long sender, string envName)
+        {
+            if (!Valid) return;
+
+            if (!this.znv.IsOwner()) return;
+
+            if (envName == string.Empty) ToggleActive(false);
+
+            this.znv.GetZDO().Set(zdo_env, envName);
+
+            znv.InvokeRPC(nameof(RPC_UpdateStone));
+        }
+
+        public bool UseItem(Humanoid user, ItemDrop.ItemData item)
+        {
+            return false;
+        }
+
+        public string GetText()
+        {
+            if (!Valid) return string.Empty;
+
+            return znv.GetZDO().GetString(zdo_env, string.Empty);
+        }
+    }
+}
---
+++ WeatherStones/Main.cs
@@ -0,0 +1,116 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace WeatherStones
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string MODNAME = "WeatherStones";
+        public const string AUTHOR = "GoldenJude";
+        public const string GUID = AUTHOR + "_" + MODNAME;
+        public const string VERSION = "0.1.0";
+
+        public static ManualLogSource log;
+
+        public static ConfigEntry<int> radius;
+        public static ConfigEntry<string> station;
+        public static ConfigEntry<string> requirements;
+
+        public static ConfigSync configSync = new ConfigSync(GUID) { DisplayName = MODNAME, CurrentVersion = VERSION, IsLocked = true };
+
+        void Awake()
+        {
+            log = Logger;
+
+            new Harmony(GUID).PatchAll(Assembly.GetExecutingAssembly());
+
+            radius = config("function", "radius", 100, "radius in which set weather applies");
+            //station = Config.Bind("", "", "piece_workbench", "");
+            //requirements = Config.Bind("", "", "Stone:30, GreydwarfEye:20, SurtlingCore:1", "");
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+    }
+
+    [HarmonyPatch(typeof(EnvMan), nameof(EnvMan.Awake))]
+    static class AddEnvPatch
+    {
+        private static void Postfix(EnvMan __instance)
+        {
+            var env = __instance.m_environments.Find(x => x.m_name.Equals("Crypt", System.StringComparison.Ordinal));
+
+            __instance.m_environments.Add(env);
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
+    static class AddPrefabs
+    {
+        private static void Postfix(ZNetScene __instance)
+        {
+            var bundle = AssetBundle.LoadFromStream(new MemoryStream(Properties.Resources.weatherstone));
+
+            var stone_big = bundle.LoadAsset<GameObject>("WS_stone_big");
+            stone_big.FixReferences();
+
+            var hammer = __instance.GetPrefab("Hammer");
+            var hammerPieces = hammer.GetComponent<ItemDrop>()?.m_itemData.m_shared.m_buildPieces.m_pieces;
+
+            if(!hammerPieces.Exists(p => p.name.Equals(stone_big.name, System.StringComparison.Ordinal))) hammerPieces.Add(stone_big);
+            if (!__instance.m_prefabs.Exists(p => p.name.Equals(stone_big.name, System.StringComparison.Ordinal))) __instance.m_prefabs.Add(stone_big);
+            if (!__instance.m_namedPrefabs.ContainsKey(stone_big.name.GetStableHashCode())) __instance.m_namedPrefabs.Add(stone_big.name.GetStableHashCode(), stone_big);
+
+            //recipe
+            var reqs = new List<Piece.Requirement>();
+            reqs.Add(new Piece.Requirement()
+            {
+                m_resItem = __instance.GetPrefab("Stone").GetComponent<ItemDrop>(),
+                m_amount = 20,
+                m_recover = true
+            });
+            reqs.Add(new Piece.Requirement()
+            {
+                m_resItem = __instance.GetPrefab("GreydwarfEye").GetComponent<ItemDrop>(),
+                m_amount = 10,
+                m_recover = true
+            });
+            reqs.Add(new Piece.Requirement()
+            {
+                m_resItem = __instance.GetPrefab("SurtlingCore").GetComponent<ItemDrop>(),
+                m_amount = 1,
+                m_recover = true
+            });
+            stone_big.GetComponent<Piece>().m_resources = reqs.ToArray();
+            stone_big.GetComponent<Piece>().m_craftingStation = __instance.GetPrefab("piece_workbench").GetComponent<CraftingStation>();
+
+            bundle.Unload(false);
+        }
+    }
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.Start))]
+    public static class AddLocalization
+    {
+        static void Postfix()
+        {
+            Localization.instance.AddWord("WS_stone_big", "Weather stone");
+        }
+    }
+}
